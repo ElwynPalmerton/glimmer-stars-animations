1dc489ace0955d01eeee39fd3282b1a09be05cc7
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2021-01-18 01:54:03 -0500

    Initial commit

--- sketch.js
+++ sketch.js
@@ -4,32 +4,35 @@
 
 //Stars 2
 
-let stars = []
+let stars = [];
+let stars2 = [];
 let speed;
 let x;
+let c;
+let colorNum = 180;
+let qty = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight); //screenWidth, screenHeight);
   // fullScreen(true);
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < qty; i++) {
     stars[i] = new Star();
-  };
+  }
+  for (let i = 0; i < qty; i++) {
+    stars2[i] = new Star(107);
+  }
 }
 
 function draw() {
   speed = 10;
-  background(20, 20, 50);
+  background(40, 20, 50);
   translate(width / 2, height / 2);
 
-
   for (let i = 0; i < stars.length; i++) {
-    stars[i].update()
-    stars[i].changeColor();
+    stars[i].update();
     stars[i].show();
+    stars2[i].update();
+    stars2[i].show();
     // star[i].setColor();
-
   }
-
-
-
 }
--- stars.js
+++ stars.js
@@ -3,47 +3,38 @@
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/17WoOqgXsRM
 
-
-function Star() {
-  this.x = random(-width / 2, width / 2);
-  this.y = random(-height / 2, height / 2);
-  this.r = 5;
-
-  let c, colorString, colorNum = 0;
-
-  this.update = function () {
-    this.r = this.r + random(-1, 1);
-
-
-  };
-
-  this.changeColor = function () {
-    this.color += 1;
-    console.log(this.color);
-    if (this.color = 255) {
-      this.color = 0;
-    };
-
+class Star {
+  constructor(colorNum) {
+    this.x = random(-width / 2, width / 2);
+    this.y = random(-height / 2, height / 2);
+    this.r = 10;
+    this.colorNum = colorNum || 10;
   }
 
-  this.show = function () {
+  update() {
+    this.r = this.r + random(-1, 1);
+  }
+
+  show() {
+    // colorString,
+
     //colorString = "HSLA(" + 350 + ", 60%, 70%, .4)";
 
     //colorString = "HSLA(50, 60%, 70%, .4)";
 
-    colorNum = this.color;
-    c = color("HSLA(" + colorNum + ", 60%, 70%, .4)");
+
+    c = color("HSLA(" + this.colorNum + ", 90%, 70%, .4)");
 
     fill(c); //this.color, this.color - 50, this.color + 100);
 
     var sx = this.x;
     var sy = this.y;
 
-    r = this.r;
+    //r = this.r;
 
     noStroke();
-    ellipse(sx, sy, r, r);
+    ellipse(sx, sy, this.r, this.r);
 
     this.pz = this.z;
-  };
+  }
 }
